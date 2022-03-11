df5e09357d03586d4670e6e883b9160eec6fe45a
Buvaneswaran S <buvaneshs2001@gmail.com>
2022-03-11 12:47:44 +0530

    NoteBook Updated

--- app/src/main/java/com/example/task_2_notes/displaycard/DisplayCardFragment.kt
+++ app/src/main/java/com/example/task_2_notes/displaycard/DisplayCardFragment.kt
@@ -1,10 +1,12 @@
 package com.example.task_2_notes.displaycard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.task_2_notes.R
 import com.example.task_2_notes.databinding.FragmentDisplayCardBinding
@@ -17,6 +19,10 @@ class DisplayCardFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_display_card, container, false)
+
+        binding.saveBtn.setOnClickListener {
+            Log.i("Testing","Data SAved")
+        }
 
         return binding.root
     }
---
+++ app/src/main/java/com/example/task_2_notes/displaycard/diaplayNoteViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.task_2_notes.displaycard
+
+import androidx.lifecycle.ViewModel
+
+class diaplayNoteViewModel: ViewModel() {
+
+    init {
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+}
